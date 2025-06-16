e7f8c3e288b0118491b83d914b592667bea3e13d
taza53 <smurtaza.53@outlook.com>
2025-06-16 18:56:53 +0200

    sep approute

--- src/main.rs
+++ src/main.rs
@@ -19,23 +19,29 @@ mod extractor;
 /// 
 /// Each variant represents a different URL pattern that can be matched by the router. If that pattern is matched,
 /// the components for that route will be rendered.
-#[derive(Debug, Clone, Routable, PartialEq)]
+// #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
-enum Route {
-    // The layout attribute defines a wrapper for all routes under the layout. Layouts are great for wrapping
-    // many routes with a common UI like a navbar.
-    #[layout(Navbar)]
-        // The route attribute defines the URL pattern that a specific route matches. If that pattern matches the URL,
-        // the component for that route will be rendered. The component name that is rendered defaults to the variant name.
-        #[route("/")]
-        Home {},
-        // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
-        // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
-        #[route("/blog/:id")]
-        // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
-        // an `id` prop of type `i32`.
-        Blog { id: i32 },
-}
+
+mod approute;
+
+// enum Route {
+//     // The layout attribute defines a wrapper for all routes under the layout. Layouts are great for wrapping
+//     // many routes with a common UI like a navbar.
+//     #[layout(Navbar)]
+//         // The route attribute defines the URL pattern that a specific route matches. If that pattern matches the URL,
+//         // the component for that route will be rendered. The component name that is rendered defaults to the variant name.
+//         #[route("/")]
+//         Home {},
+//         // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
+//         // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
+//         #[route("/blog/:id")]
+//         // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
+//         // an `id` prop of type `i32`.
+//         Blog { id: i32 },
+//         // The route for seo analysis
+//         gi#[route("/seo")]
+//         HeadingExtractor {}
+// }
 
 // We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
 // The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
@@ -67,7 +73,7 @@ fn App() -> Element {
         // The router component renders the route enum we defined above. It will handle synchronization of the URL and render
         // the layouts and components for the active route.
         Router::<Route> {}
-        HeadingExtractor{}
+        // HeadingExtractor{}
     }
 
 }
