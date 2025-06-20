be364c55c07975693530e5a4439497b573965e0a
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-06-20 21:42:19 +0200

    Remove  CSS files, they were messing up other stuff

--- src/components/echo.rs
+++ src/components/echo.rs
@@ -1,7 +1,5 @@
 use dioxus::prelude::*;
 
-const ECHO_CSS: Asset = asset!("/assets/styling/echo.css");
-
 /// Echo component that demonstrates fullstack server functions.
 #[component]
 pub fn Echo() -> Element {
@@ -13,7 +11,6 @@ pub fn Echo() -> Element {
     let response = use_signal(|| String::new());
 
     rsx! {
-        document::Link { rel: "stylesheet", href: ECHO_CSS }
 
         div {
             // id: "echo",
