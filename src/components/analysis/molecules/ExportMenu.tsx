acfed700486a9cfaf944653132846770fa664738
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-12-19 02:13:19 +0100

    extract exportmenu into seperate component

---
+++ src/components/analysis/molecules/ExportMenu.tsx
@@ -0,0 +1,52 @@
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+} from "@radix-ui/react-dropdown-menu"
+import {
+    Download,
+    ChevronDown,
+    FileText,
+    TableIcon,
+} from "lucide-react"
+import { Button } from "../../ui/button"
+
+export function ExportMenu({
+    onPDF,
+    onText,
+    onCSV,
+}: {
+    onPDF: () => void
+    onText: () => void
+    onCSV: () => void
+}) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="shrink-0 bg-transparent">
+                    <Download className="h-4 w-4 mr-2" />
+                    Export Report
+                    <ChevronDown className="h-4 w-4 ml-2" />
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={onPDF}>
+                    <FileText className="h-4 w-4 mr-2" />
+                    Download PDF
+                </DropdownMenuItem>
+
+                <DropdownMenuItem onClick={onText}>
+                    <FileText className="h-4 w-4 mr-2" />
+                    Download Text Report
+                </DropdownMenuItem>
+
+                <DropdownMenuItem onClick={onCSV}>
+                    <TableIcon className="h-4 w-4 mr-2" />
+                    Download CSV Data
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
